eb841edef8a55bbab9d081cbe6994efb3f3a2bad
sheltonFarias <sheltonm10@outlook.com>
2025-07-31 14:19:29 -0300

    first commit

---
+++ if/exercicio1-facil/explicacao.md
@@ -0,0 +1,3 @@
+1. **Par ou Ímpar**
+    
+    Peça ao usuário que digite um número inteiro. Verifique se o número é par ou ímpar e exiba a mensagem correspondente.
---
+++ if/exercicio1-facil/script.js
@@ -0,0 +1,13 @@
+function numeroPar(number) {
+  if(number % 2 == 0) {
+    console.log("Esse numero é Par")
+  } else {
+    console.log("Esse número é impar")
+  }
+}
+
+numeroPar(7)
+numeroPar(2)
+
+
+/* % simbolo usado para saber o quociente da divisão entre os numeros citados */
