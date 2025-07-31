ccbc5b84fd8febf4dadd26f43346279055bd385d
sheltonFarias <sheltonm10@outlook.com>
2025-07-31 14:39:26 -0300

    resolve second exercise if

---
+++ if/exercicio2-facil/explicacao.md
@@ -0,0 +1,3 @@
+2. **Maioridade**
+    
+    Solicite a idade do usuário. Informe se ele é maior ou menor de idade (considerando 18 anos como o limite).
---
+++ if/exercicio2-facil/script.js
@@ -0,0 +1,11 @@
+function maioridade(idade) {
+  if(idade >= 18) {
+    console.log("Você é maior de idade")
+  } else {
+    console.log("Você é menor de idade")
+  }
+}
+
+maioridade(17)
+
+/* usado >= para compara com idade passada na função assim retornado a mensagem */
